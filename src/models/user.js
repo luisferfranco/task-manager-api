33e900af56fe568487f1e74a96333149620707f8
luisferfranco <luisferfranco@gmail.com>
2022-03-12 12:22:13 -0600

    Envío de JSON WebTokens

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -49,7 +50,26 @@ const userSchema = new mongoose.Schema({
       }
     },
   },
+  tokens: [
+    {
+      token: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
 });
+
+// Los estáticos se ejecutan en las clases, los métodos en las instancias
+userSchema.methods.generateAuthToken = async function () {
+  const user = this;
+  const token = jwt.sign({ _id: user._id.toString() }, "ThisIsMySecret");
+
+  user.tokens = user.tokens.concat({ token });
+  await user.save();
+
+  return token;
+};
 
 userSchema.statics.findByCredentials = async (email, password) => {
   const hashedPassword = await bcrypt.hash(password, 8);
--- src/routers/users.js
+++ src/routers/users.js
@@ -7,7 +7,8 @@ router.post("/users", async (req, res) => {
 
   try {
     await user.save();
-    res.status(200).send(user);
+    const token = user.generateAuthToken();
+    res.status(200).send({ user, token });
   } catch (e) {
     res.status(400).send(e);
   }
@@ -90,7 +91,9 @@ router.post("/users/login", async (req, res) => {
       req.body.email,
       req.body.password
     );
-    res.send(user);
+    const token = await user.generateAuthToken();
+
+    res.send({ user, token });
   } catch (error) {
     res.status(400).send();
   }
