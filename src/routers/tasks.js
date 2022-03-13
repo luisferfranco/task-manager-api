b4950929cbcf921200f832888c6b81cc0f9b1bcb
luisferfranco <luisferfranco@gmail.com>
2022-03-12 23:04:19 -0600

    funcionalidad de las rutas de tasks considerando al user

--- src/routers/tasks.js
+++ src/routers/tasks.js
@@ -17,20 +17,20 @@ router.post("/tasks", auth, async (req, res) => {
   }
 });
 
-router.get("/tasks", async (req, res) => {
+router.get("/tasks", auth, async (req, res) => {
   try {
-    const tasks = await Task.find({});
-    res.status(201).send(tasks);
+    await req.user.populate("tasks").execPopulate();
+    res.status(201).send(req.user.tasks);
   } catch (e) {
     res.status(500).send(e);
   }
 });
 
-router.get("/tasks/:id", async (req, res) => {
+router.get("/tasks/:id", auth, async (req, res) => {
   const _id = req.params.id;
 
   try {
-    const task = await Task.findById(_id);
+    const task = await Task.findOne({ _id, owner: req.user._id });
     if (!task) {
       return res.status(404).send();
     }
@@ -40,7 +40,7 @@ router.get("/tasks/:id", async (req, res) => {
   }
 });
 
-router.patch("/tasks/:id", async (req, res) => {
+router.patch("/tasks/:id", auth, async (req, res) => {
   // Verificar que solo podamos cambiar ciertos campos
   const updates = Object.keys(req.body);
   const allowedUpdate = ["completed", "description"];
@@ -54,7 +54,10 @@ router.patch("/tasks/:id", async (req, res) => {
 
   // Buscar y actualizar
   try {
-    const task = await Task.findById(req.params.id);
+    const task = await Task.findOne({
+      _id: req.params.id,
+      owner: req.user._id,
+    });
 
     if (!task) {
       return res.status(404).send();
@@ -70,13 +73,18 @@ router.patch("/tasks/:id", async (req, res) => {
   }
 });
 
-router.delete("/tasks/:id", async (req, res) => {
+router.delete("/tasks/:id", auth, async (req, res) => {
   try {
-    const task = await Task.findByIdAndDelete(req.params.id);
+    const task = await Task.findOne({
+      _id: req.params.id,
+      owner: req.user._id,
+    });
 
     if (!task) {
       return res.status(404).send();
     }
+
+    task.remove();
 
     return res.send(task);
   } catch (error) {
